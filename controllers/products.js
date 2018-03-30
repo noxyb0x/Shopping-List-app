d916625f828fe0072e0d80da8cf35b71502a35dd
noxyb0x <nerminm16@gmail.com>
2018-03-30 23:22:57 +0200

    implemented form validation, search by category on image click, animations and fixed some misc. bugs

--- controllers/products.js
+++ controllers/products.js
@@ -1,54 +1,73 @@
-var app = angular.module('ShoppingListModule', []);
-
-app.controller('ProductListController', ['$http', '$scope', function ($http, $scope) {
-    $scope.products = [];
-    $scope.total = 0;
-    //calculate total cost ammount function
-    $scope.calcTotal = function (products) {
+var app = angular.module('ShoppingListModule', ['ngAnimate'])
+    .controller('ProductListController', ['$http', '$scope', function ($http, $scope) {
         $scope.total = 0;
-        for (let product of products)
-            $scope.total += product.quantity * product.cost;
-    }
-    //get all products from API and save current state (use factories for prod. apps!)
-    $scope.getAllprods = function () {
+        //calculate total cost ammount function
+        $scope.calcTotal = function (products) {
+            let temp = 0;
+            for (let product of products)
+                temp += product.quantity * product.cost;
+            $scope.total = temp;
+        }
+        // get all products from API and save current state
         $http.get('/api/products').then(
             (res) => {
                 $scope.products = res.data;
                 $scope.calcTotal($scope.products);
-                console.log(res.data);
             },
-            (res) => {
-                console.log(res.status + " " + res.statusText);
+            (err) => {
+                console.log(err.status + " " + err.statusText);
             });
-    };
-    $scope.getAllprods();
-
-    //make an api call to add a product into the database 
-    $scope.formData = {};
-    $scope.addProduct = function () {
-        $http.post('/api/products', $scope.formData).then(
-            (res) => {
-                console.log('Success ' + res.status);
-                $scope.getAllprods();
-                $scope.formData = {};
-            },
-            (res) => {
-                console.log(res.status + " " + res.statusText);
-            }
-        )
-    };
-
-    //make an api call to delete a product from the database
-    $scope.deleteProduct = function (id) {
-        $http.delete('/api/products/' + id).then(
-            (res) => {
-                console.log('Success ' + res.status);
-                $scope.getAllprods();
-            },
-            (res) => {
-                console.log('Error ' + res.status);
-            }
-        )
-    };
-
-}]);
+        //frontend validate if same product entered twice
+        var findSameAndUpdate = function (newP) {
+            for (let prod of $scope.products)
+                if (prod.name === newP.name && prod.category === newP.category) {
+                    prod.quantity += newP.quantity;
+                    prod.cost = newP.cost;
+                    return true;
+                }
+            return false;
+        }
+        //make an api call to add a product into the database 
+        $scope.formData = {};
+        $scope.addProduct = function (form) {
+            $http.post('/api/products', $scope.formData).then(
+                (res) => {
+                    if (!findSameAndUpdate($scope.formData))
+                        $scope.products.push($scope.formData);
+                    $scope.calcTotal($scope.products);
+                    $scope.formData = {};
+                },
+                (err) => {
+                    console.log(err.status + " " + err.statusText);
+                }
+            )
+            //form validation cleanup
+            form.$setPristine();
+            form.$setUntouched();
+        };
+        //remove product from scope
+        var findAndRemoveP = function (Pname, Pcategory) {
+            for (let product of $scope.products)
+                if (product.name === Pname && product.category === Pcategory)
+                    $scope.products.splice($scope.products.indexOf(product), 1);
+        }
+        //make an api call to delete a product from the database
+        $scope.deleteProduct = function (id, name, category) {
+            $http.delete('/api/products/' + id).then(
+                (res) => {
+                    findAndRemoveP(name, category);
+                    $scope.calcTotal($scope.products);
+                },
+                (err) => {
+                    console.log(err.status + " " + err.status);
+                }
+            )
+        };
+        //misc - there's probably a hundred better ways to do this, but I'm tired and don't care atm.
+        $scope.appendToSearchBox = function (imgCategory) {
+            if ($scope.searchBox === imgCategory)
+                $scope.searchBox = '';
+            else
+                $scope.searchBox = imgCategory;
+        }
+    }]);
